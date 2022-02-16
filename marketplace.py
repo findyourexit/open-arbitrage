0ad622d3db7a7585cbf5d4643800be88214f5674
Tom Larcher <t.larcher@forcite.com.au>
2022-02-16 15:28:31 +1100

    Reimplemented market fluctuation (reroll) using Gaussian distribution, instead of uniformed distribution

--- marketplace.py
+++ marketplace.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Item:
     def __init__(self, name, value):
         self.name = name
@@ -5,3 +8,16 @@ class Item:
         self.min_value = int(value * 0.5)
         self.max_value = int(value * 1.5)
         self.last_value = self.value
+
+
+def fluctuate_market(market):
+    for item in market:
+        item.last_value = item.value
+        # reroll = item.value * random.uniform(0.5, 1.5)
+        reroll = item.value * random.gauss(0.95, 0.95/2)
+        if reroll < item.min_value:
+            item.value = item.min_value
+        elif reroll > item.max_value:
+            item.value = item.max_value
+        else:
+            item.value = reroll
